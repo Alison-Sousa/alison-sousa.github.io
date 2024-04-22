a2875e6f0c080e5074bb54374fb20169855c8905
Alison-Sousa <116979938+Alison-Sousa@users.noreply.github.com>
2024-04-22 17:05:35 -0300

    Update pages.js

--- styles/pages.js
+++ styles/pages.js
@@ -1,38 +1,38 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Seleciona todos os links de navegação
+    // Selects all navigation links
     const navLinks = document.querySelectorAll('.nav-item .nav-link');
   
-    // Função para carregar conteúdo das abas
+    // Function to load tab content
     const loadContent = (id) => {
-      // Constrói o caminho do arquivo baseado no id
+      // Constructs the file path based on the id
       const filePath = id + ".html";
       fetch(filePath)
         .then(response => response.text())
         .then(html => {
-          // Seleciona a seção de conteúdo baseada no id e injeta o HTML
+          // Selects the content section based on the id and injects the HTML
           document.getElementById(id + 'Content').innerHTML = html;
           document.getElementById(id + 'Content').style.display = 'block';
         })
         .catch(error => console.error('Error loading content:', error));
     };
   
-    // Função para manipular clique na aba
+    // Function to handle tab click
     const handleTabClick = (event) => {
-      event.preventDefault(); // Prevenir o comportamento padrão do link
+      event.preventDefault(); // Prevents the default behavior of the link
       const clickedTabId = event.target.id;
   
-      // Esconde todas as seções de conteúdo e remove a classe 'active'
+      // Hides all content sections and removes the 'active' class
       document.querySelectorAll('.content-section').forEach(section => section.style.display = 'none');
       navLinks.forEach(link => link.parentElement.classList.remove('active'));
   
-      // Adiciona 'active' ao item clicado da navegação
+      // Adds 'active' to the clicked navigation item
       event.target.parentElement.classList.add('active');
   
-      // Carrega o conteúdo para a aba selecionada
+      // Loads content for the selected tab
       loadContent(clickedTabId);
     };
   
-    // Adiciona evento de clique a cada link da navegação
+    // Adds click event to each navigation link
     navLinks.forEach(link => {
       link.addEventListener('click', handleTabClick);
     });
